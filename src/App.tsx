916566bc045e166a33efc4a70d2dd39e6d703406
Joshua Fernandes <fernandes.josh@gmail.com>
2022-12-05 17:39:10 -0500

    style: typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-2 dark:bg-slate-700 dark:text-white">
-      <h1 className="text-3xl font-bold">Just Another Initative Tracker</h1>
+      <h1 className="text-3xl font-bold">Just Another Initiative Tracker</h1>
     </div>
   );
 }
